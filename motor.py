a8656c4d8b29826d98197c6d310ad29e838be59f
michaeljfriedman <mfred240@gmail.com>
2016-11-12 14:14:08 +0000

    Implemented Motor class

--- motor.py
+++ motor.py
@@ -8,13 +8,18 @@ hardware and provides a simple interface for clients to use.
 
 from Tkinter import *
 import RPi.GPIO as GPIO
+import threading
 import time
-from motor import Motor
 
 class Motor:
 	
+	# Public constants
 	FORWARD = True
-	BACKWARD = not FORWARD
+	BACKWARD = False
+	
+	# Public constants
+	_DC_BACKWARD = 99
+	_DC_FORWARD = 6
 	
 	'''
 	Creates a new instance of a Motor object
@@ -25,11 +30,51 @@ class Motor:
 			direction is reversed
 	'''
 	def __init__(self, channel_out, flipped=False):
-		self._reversed = flipped
+		self._flipped = flipped
+		self._is_started = False
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(channel_out, GPIO.OUT)
 		self._pwm = GPIO.PWM(channel_out, 100)
-		self._pwm.start(5)
+		self._pwm.start(0)
+		
+	
+	'''
+	Helper method for _rotate()
+	
+	Executes one forward motion cycle.
+	'''
+	def _rotate_forward(self, speed):
+		# TODO: Use speed somehow
+		self._pwm.ChangeDutyCycle(Motor._DC_FORWARD)
+			
+			
+	'''
+	Helper method for _rotate()
+	
+	Executes one forward motion cycle.
+	'''
+	def _rotate_backward(self, speed):
+		# TODO: Use speed somehow
+		self._pwm.ChangeDutyCycle(Motor._DC_BACKWARD)
+			
+	
+	'''
+	Helper method for start()
+	
+	Contains main loop for motor rotation
+	'''
+	def _rotate(self, speed, direction):
+		while self._is_started:
+			if direction == Motor.FORWARD:
+				if self._flipped:
+					self._rotate_backward(speed)
+				else:
+					self._rotate_forward(speed)
+			else:
+				if self._flipped:
+					self._rotate_forward(speed)
+				else:
+					self._rotate_backward(speed)
 	
 	'''
 	Spins motor at an optionally specified (relative) speed
@@ -38,10 +83,27 @@ class Motor:
 		speed: int from 1 to 5 indicating speed
 		direction: boolean indicating forward or backward motion
 	'''
-	def start(self, speed=1, direction=Motor.FORWARD):
-		while True:
-			for dc in range(0, 101, 5):
-				self._pwm.ChangeDutyCycle(dc)
-				time.sleep(0.1)
+	def start(self, speed=1, direction=FORWARD):
+		if self._is_started:
+			raise Exception('Motor already started. Must check with is_started() before calling start()')
+		
+		self._is_started = True
+		task = self._rotate
+		t = threading.Thread(target=task, args=(speed, direction))
+		t.start()
 
+
+	'''
+	Stops spinning the motor
+	'''
+	def stop(self):
+		self._is_started = False
+		
 	
+	'''
+	Returns True if the motor has been started - i.e. if start() was
+	called without a corresponding call to stop()
+	'''
+	def is_started(self):
+		return self._is_started
+		
